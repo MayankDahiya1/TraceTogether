6da74d5063df2ff865cac47004df73f2e9b8d183
Mayank Dahiya <dahiyamayank0@gmail.com>
2021-07-14 16:28:57 +0530

    New Files

--- app.js
+++ app.js
@@ -30,7 +30,11 @@ app.get("/resources", function(req,res){
 app.get("/contact", function(req,res){
   res.render("contact");
 });
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-  app.listen(3000, function() {
-    console.log("Server started on port 3000");
+  app.listen(port, function() {
+    console.log("Server started Successfully");
   });
