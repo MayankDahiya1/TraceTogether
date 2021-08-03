000cfcfe10098c52460dd619655a0224bf70283b
Mayank Dahiya <dahiyamayank0@gmail.com>
2021-08-03 15:14:29 +0530

    New Files Added 03=08-2021

--- app.js
+++ app.js
@@ -4,18 +4,30 @@ const ejs = require("ejs");
 const https = require("https");
 const mongoose = require('mongoose');
 const state="Search Your State";
+const stateNames =[];
+const tableConfirmed=[];
+const tableActive=[];
+const tableRecovered=[];
+const tableDeceased=[];
+
+const states =[];
 const app = express();
+const err = "Please Search Correct State";
 const commaNumber = require("comma-number");
+const err1= "Error";
 
 app.set('view engine', 'ejs');
 const action = "active"; 
-const confirmed1 =[];
-const deaths1=[];
-const recovered1=[];
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+var today = new Date();
+var yesterday = new Date(today);
+yesterday.setDate(today.getDate() - 1);
+var date = yesterday.toISOString().slice(0,10);
 
 app.get("/",function(req,res){
+
   const url = "https://covid19-stats-api.herokuapp.com/api/v1/cases?country=India";
   https.get(url, function(response){
       response.on("data",function(data){
@@ -23,12 +35,40 @@ app.get("/",function(req,res){
           let confirmed = commaNumber(covidData.confirmed);
           let deaths = commaNumber(covidData.deaths);
           let recovered =commaNumber( covidData.recovered);
-          res.render("home",{confirmed1:confirmed, deaths1:deaths, recovered1:recovered,state:state});
+          res.render("home",{confirmed1:confirmed, deaths1:deaths, recovered1:recovered,state:state,stateNames:stateNames,tableConfirmed:tableConfirmed,tableActive:tableActive,tableDeceased:tableDeceased,tableRecovered:tableRecovered});
 
       });
-
+ 
+  
      
   });
+  const url3="https://covid-api.com/api/reports?date="+date+"&q=India&iso=IND&region_name=India";
+  https.get(url3,function(response){
+    response.on("data",function(data3){
+      let table = JSON.parse(data3);
+      for(let i=0; i<37;i++){
+        let stateName = table.data[i].region.province;
+        stateNames.push(stateName);
+        let tableconfirmed = table.data[i].confirmed;
+        tableConfirmed.push(tableconfirmed);
+        let tableactive =table.data[i].active;
+        tableActive.push(tableactive);
+        let tablerecovered = table.data[i].recovered;
+        tableRecovered.push(tablerecovered);
+        let tabledeceased = table.data[i].deaths;
+        tableDeceased.push(tabledeceased);
+
+      }
+    
+      
+      
+      
+      
+      
+
+});
+
+});
 });
 function clearArray(array) {
   while (array.length) {
@@ -38,7 +78,7 @@ function clearArray(array) {
 app.post("/", function(req,res){
   let state = req.body.search;
   
-  const url1 = "https://covid-api.com/api/reports?date=2021-07-20&q=India&iso=IND&region_name=India&region_province="+state+"";
+  const url1 = "https://covid-api.com/api/reports?date="+date+"&q=India&iso=IND&region_name=India&region_province="+state+"";
   
   https.get(url1, function(response){
       response.on("data",function(data1){
@@ -48,13 +88,20 @@ app.post("/", function(req,res){
           let deathsState = commaNumber(covidStateData.data[0].deaths);
           let recoveredState = commaNumber(covidStateData.data[0].recovered);
 
-          res.render("home",{confirmed1:confirmedState, deaths1:deathsState, recovered1:recoveredState,state:state});
-           
+          res.render("home",{confirmed1:confirmedState, deaths1:deathsState, recovered1:recoveredState,state:state,stateNames:stateNames,tableConfirmed:tableConfirmed,tableActive:tableActive,tableDeceased:tableDeceased,tableRecovered:tableRecovered});
+
+      });
+    
+          
+          
+        });
+        
       });
       
       
-  });
-});
+       
+  
+
 
 
 
